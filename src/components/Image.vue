ad27cff191cad78a3ecec77e1fea61325f916c4c
manupuyol <manuelpuyol.dev@gmail.com>
2022-12-10 20:02:36 +0100

    [feat] loading:added loader while images loading

--- src/components/Image.vue
+++ src/components/Image.vue
@@ -5,7 +5,7 @@ const { customsrc } = props;
 </script>
 
 <template>
-  <div class="col-3 d-flex flex-column">
+  <div class="col-xxl-2 col-lg-3 col-md-4 col-6 d-flex flex-column p-1">
     <VLazyImage :src="customsrc" class="img-fluid" />
   </div>
 </template>
